f5d6a196e84fe4262a338702d0c405c3260ebb73
Peter Gunarso <peter@gunar.so>
2019-03-12 18:46:42 -0700

    make q1 interactive

---
+++ my_ui.R
@@ -0,0 +1,56 @@
+library("shiny")
+library("ggplot2")
+library("tidyr")
+
+my_ui <- fluidPage(
+  titlePanel(strong("Final Project")),
+  
+  mainPanel(
+    tabsetPanel(type = "tabs",
+                tabPanel(
+                  'Introduction'
+                ),
+                tabPanel(
+                  "Freshman Courses",
+                  sidebarLayout(
+                    sidebarPanel(
+                      selectInput(inputId = 'q1_select', label = 'Subsection', 
+                                  choices = c('Introduction', 'Computer Science', 'Business', 'Conclusion'), 
+                                              selected = 'Introduction'),
+                      br(),
+                      
+                      conditionalPanel("input.q1_select == 'Computer Science' || input.q1_select == 'Business'",
+                                       selectInput(inputId = 'q1_type', label = 'Type', choices = c('Type 1', 'Type 2'),
+                                                   selected = 'Type 1'))
+                    ),
+                    mainPanel(
+                      htmlOutput(outputId = 'q1_page'), br(),
+                      conditionalPanel("input.q1_select == 'Computer Science' || input.q1_select == 'Business'",
+                        htmlOutput(outputId = 'q1_subsec')
+                      )
+                    )
+                  )
+                ),
+                tabPanel(
+                  "Easiest Departments", 
+                  p("blah blah blah")
+                ),
+                tabPanel(
+                  "Hardest Departments", 
+                  p("blah blah blah")
+                ),
+                tabPanel(
+                  "Types of Teachers", 
+                  p("blah blah blah")
+                ),
+                tabPanel(
+                  "Class Size", 
+                  img(src = "img/q4.png", width = 600, height = 400, alt = "Average GPA per class size per school"),
+                  p("Both the University of Washington and Virginia Tech have decreasing average GPA's the larger the class size gets. This suggests that class size and GPA are negatively correlated. Although the average GPA drops for both schools as class size increases, the University of Washington had consistently higher averages for each class size, and didn't see as big of a gap between class sizes. From the smallest class size to the largest, the University of Washington only saw a .098 point drop in average GPA, while Virginia Tech saw a .272 point drop in average GPA."), 
+                  br(),
+                  p("The decreasing average GPA could be the result of multiple factors. Easier classes will typicaly have higher GPA's and often times larger classes are general introduction courses and are therefore easier than their smaller, more specified counterparts. Larger courses may also have more resources (Teachers, Teaching Assistants, Office Hours, Study Centers) for them, making it easier to do well in class. Additionally, the range of student abilities within larger classes is typically much wider, limiting the scope of the class and the material covered. This in turn lowers expectations and leads to easier assignments, midterms and finals.")
+                  
+                )
+    )
+  )
+)
