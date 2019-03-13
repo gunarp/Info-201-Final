059cfc8e27d19f0ac61662fb59897cdc4e4bd548
mbodzewski <michanbc9@gmail.com>
2019-03-12 23:54:36 -0700

    add question 2 into shiny app

--- my_ui.R
+++ my_ui.R
@@ -4,6 +4,9 @@ library("tidyr")
 
 my_ui <- shinyUI(navbarPage(
   "App Title",
+  tabPanel(
+    "Introduction"
+  ),
   tabPanel("Class Size",
            sidebarPanel(selectInput(
              inputId = "subject", 
@@ -31,9 +34,57 @@ my_ui <- shinyUI(navbarPage(
                This in turn lowers expectations and leads to easier assignments, midterms and finals.")
            )
            ),
-  tabPanel("Hardest Classes",
-           sidebarPanel(),
-           mainPanel()
-           )
+  tabPanel(
+    
+    "High GPA Departments", 
+    selectInput(inputId = "school_select_high", 
+                label = "School of Interest:", 
+                choices = c("University of Washington", "Virginia Tech"),
+                selected = "University of Washington"
+    ),
+    conditionalPanel(
+      "input.school_select_high == 'University of Washington'",
+      p("This graph displays the top 10 departments at the at the University of Washington 
+        where  a number of classes in each of these departments have an average GPA at or above a 3.8. 
+        From this graph the English department and Sociology departments have the most number
+        of classes with 20 or more classes where students got high averages."),
+      plotOutput("uw_high")
+      ),
+    conditionalPanel(
+      "input.school_select_high == 'Virginia Tech'",
+      p("This graph displays the top 10 departments at Virginia Tech where the
+        number of classes in each of these departments have an average GPA at or above a 3.8 and while 
+        none of the departments have more than 20 classes where students get high averages, 
+        the distribution is less as more than all 6 of the departments shown have more than
+        10 classes where the average GPA was or above a 3.8. However, for Virginia Tech, their 
+        top departments are Education Curriculum and Instruction and English. "),
+      plotOutput("vt_high")
+      )                  
+    ),
+  tabPanel(
+    "Low GPA Departments", 
+    selectInput(inputId = "school_select_low", 
+                label = "School of Interest:", 
+                choices = c("University of Washington", "Virginia Tech"),
+                selected = "University of Washington"
+    ), 
+    conditionalPanel(
+      "input.school_select_low == 'University of Washington'",
+      p("This graph shows the top 10 departments at the University of Washington where
+                       the number of classes in each of these departments have an average GPA at or below a 2.8. 
+                       In this graph, Math has the most amount of classes at 14 while the second highest is 
+                       the Biology department which has 8 classes with a low average GPA. "),
+      plotOutput("uw_low")
+    ),
+    conditionalPanel(
+      "input.school_select_low == 'Virginia Tech'",
+      p("This graph shows the top 10 departments at Virgina Tech where the
+                       number of classes in each of these departments have an average GPA at or below a 2.8.
+                       In this graph the Math department and Electrical Computer Engineer department 
+                       have the most amount of classes which has similarities to the UW data as the
+                       Math department also had the highest number of classes with low GPAs. "),
+      plotOutput("vt_low")
+    )  
+  )
   ))
 
